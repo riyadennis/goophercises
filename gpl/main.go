74215c08a3c9425a57770435d2a9e34daa1a27a8
riyadennis <riyacv@gmail.com>
2019-11-26 16:36:26 +0000

    added go routine to process the files

--- gpl/main.go
+++ gpl/main.go
@@ -5,35 +5,49 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"sync"
 )
 type data struct{
 	cnt int
 	name string
 }
 func main(){
-	m := make(map[string]*data)
-	for _, n := range os.Args[1:]{
-		cntnt, err := ioutil.ReadFile(n)
-		if err != nil{
-			fmt.Fprintf(os.Stdout,
-				"error opening %v file %s ",  err, n)
+	ch := make(chan map[string]*data)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(){
+		for _, n := range os.Args[1:]{
+			checkDuplicate(n, ch)
 		}
-		for _, l := range strings.Split(string(cntnt), "\n"){
-			if m[l] != nil{
-				m[l].cnt = m[l].cnt+1
-			} else{
-				m[l] = &data{
-					cnt:1,
-					name:n,
-				}
-			}
-		}
-	}
+		wg.Done()
+		close(ch)
+	}()
 
-	for l, d := range m{
+	for l, d := range <-ch{
 		if d.cnt > 1{
 			fmt.Printf("duplicated line: %q, %d times in file %s\n", l, d.cnt, d.name)
 		}
 	}
+	wg.Wait()
+}
 
+func checkDuplicate(name string, ch chan map[string]*data) <-chan map[string]*data{
+	m := make(map[string]*data)
+	content, err := ioutil.ReadFile(name)
+	if err != nil{
+		fmt.Fprintf(os.Stdout,
+			"error opening %v file %s ",  err, name)
+	}
+	for _, l := range strings.Split(string(content), "\n"){
+		if m[l] != nil{
+			m[l].cnt = m[l].cnt+1
+		} else{
+			m[l] = &data{
+				cnt:1,
+				name:name,
+			}
+		}
+	}
+	ch<-m
+	return ch
 }
