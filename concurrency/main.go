af179f4fd078188d91c68e6b7a5dd95b05ee508e
Riya Dennis <riyacv@gmail.com>
2019-12-01 13:57:20 +0000

    added rob pike concurrency example

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,17 +2,56 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main() {
-	ch1 := sq(gen(2, 3, 4, 5, 6, 7))
-	ch2 := sq(gen(23, 24, 25, 26))
-	done := make(chan struct{})
-	for c := range merge(done, ch1, ch2) {
-		fmt.Printf("squares %v\n", c)
-	}
+type message struct {
+	msg  string
+	wait chan bool
+}
 
-	waitGroup()
-	jobs()
-	rangeOver()
+func main() {
+	ch := fanIn(boring("joe"), boring("ann"))
+	for i := 0; i < 10; i++ {
+		msg := <-ch
+		fmt.Printf("you say %s\n", msg.msg)
+		msg.wait <- true
+	}
+	fmt.Println("just leave")
+}
+
+func fanIn(in1 <-chan message, in2 <-chan message) <-chan message {
+	ch := make(chan message)
+	go func() {
+		for {
+			select {
+			case s := <-in1:
+				ch <- s
+			case s := <-in2:
+				ch <- s
+			case <-time.After(1 * time.Second):
+				fmt.Println("timed out")
+				return
+			}
+		}
+	}()
+	return ch
+}
+
+func boring(msg string) <-chan message {
+	c := make(chan message)
+	wait := make(chan bool)
+	go func() {
+		for i := 0; ; i++ {
+			msg := message{
+				msg:  fmt.Sprintf("%s : %d", msg, i),
+				wait: wait,
+			}
+			c <- msg
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+			<-wait
+		}
+	}()
+	return c
 }
