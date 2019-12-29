a8550d732cd5ec1af77cf37e1f488d1d541b8222
Riya Dennis <riyacv@gmail.com>
2019-12-29 11:41:35 +0000

    added response check for client

--- tcp/client.go
+++ tcp/client.go
@@ -8,50 +8,51 @@ import (
 	"os"
 )
 
+const Port = ":8085"
+
 func main() {
-	conn, err := net.Dial("tcp", ":8085")
+	conn, err := net.Dial("tcp", Port)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	userInput(conn)
-	checkResponse(conn)
+	for {
+		userInput(conn)
+		checkResponse(conn)
+	}
 }
 
 func userInput(conn net.Conn) {
-	for {
-		fmt.Print(">>")
-		userInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		switch err {
-		case nil:
-			fmt.Fprintf(conn, "%s\n", userInput)
-		case io.EOF:
-			os.Exit(0)
-		default:
-			fmt.Fprintf(conn, "ERROR")
-			os.Exit(1)
-		}
+	fmt.Print(">>")
+	userInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	switch err {
+	case nil:
+		fmt.Fprintf(conn, "%s\n", userInput)
+	case io.EOF:
+		os.Exit(0)
+	default:
+		fmt.Fprintf(conn, "ERROR")
+		os.Exit(1)
 	}
+
 }
 
 func checkResponse(conn net.Conn) {
-	for {
-		response, err := bufio.NewReader(conn).ReadString('\n')
-		switch err {
-		case nil:
-			switch response {
-			case "SUCCESS\n":
-				fmt.Println("communication successful")
-				break
-			default:
-				fmt.Println("communication not successful")
-				break
-			}
-		case io.EOF:
-			os.Exit(0)
+	response, err := bufio.NewReader(conn).ReadString('\n')
+	switch err {
+	case nil:
+		fmt.Println(response)
+		switch response {
+		case "SUCCESS\n":
+			fmt.Println("communication successful")
 		default:
-			fmt.Fprintf(conn, "ERROR")
-			os.Exit(1)
+			fmt.Println("communication not successful")
 		}
+	case io.EOF:
+		os.Exit(0)
+	default:
+		fmt.Fprintf(conn, "ERROR")
+		os.Exit(1)
 	}
+
 }
